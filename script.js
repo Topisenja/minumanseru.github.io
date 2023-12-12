04286e684e5c62e9c67baa91c78359c001f77d4e
Topisenja <teraspancapersada@gmail.com>
2023-12-12 20:47:17 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
